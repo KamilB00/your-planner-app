9bb110e92dd6d7d0175d80ab54bc725997945c35
KamilB00 <bonkowski.kamil@icloud.com>
2020-11-07 19:11:32 +0100

    dependencies added

---
+++ build/precache-manifest.c4663d6636e275420588195418aefcfb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99a75b10e58a11a1d26cd71bf0d5051",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a6fc26f58d454148204",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "ede9d884f276f0f1d0a1",
+    "url": "/static/css/main.a32da756.chunk.css"
+  },
+  {
+    "revision": "3a6fc26f58d454148204",
+    "url": "/static/js/2.f75b6e7a.chunk.js"
+  },
+  {
+    "revision": "6258d81e81986253686f445d01ab1e84",
+    "url": "/static/js/2.f75b6e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede9d884f276f0f1d0a1",
+    "url": "/static/js/main.dbb6e1b6.chunk.js"
+  },
+  {
+    "revision": "9e2d0b99415b1d6e32f6",
+    "url": "/static/js/runtime-main.d1cf1f10.js"
+  },
+  {
+    "revision": "e8659d75e31e4ab0a413b83ddba9d7de",
+    "url": "/static/media/home-image.e8659d75.jpeg"
+  },
+  {
+    "revision": "71c35017c6b9b57f947f5f8fe00eaf0c",
+    "url": "/static/media/logo512_YP.71c35017.png"
+  },
+  {
+    "revision": "ea1d6e6224ee618c70caae40aef7e78b",
+    "url": "/static/media/logo_dark.ea1d6e62.png"
+  },
+  {
+    "revision": "c55ce10b76ed5e6247edecdaa621fa13",
+    "url": "/static/media/logo_grey.c55ce10b.png"
+  },
+  {
+    "revision": "1ff7983ce498675a53dd5dce9b2ff3ea",
+    "url": "/static/media/logo_white.1ff7983c.png"
+  },
+  {
+    "revision": "5e7926626b301f8a44d96ca426422039",
+    "url": "/static/media/mac_iphone.5e792662.png"
+  },
+  {
+    "revision": "1e6416553f4cd945a4267555986bc74b",
+    "url": "/static/media/noTasksImage.1e641655.jpeg"
+  },
+  {
+    "revision": "cecd0a530086643a7c5ac932ba18021a",
+    "url": "/static/media/noTasksInFilterFound.cecd0a53.jpeg"
+  }
+]);
