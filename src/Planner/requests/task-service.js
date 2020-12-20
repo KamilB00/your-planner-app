7418ed3947ee0f79bae1f6ecff57ccd30bf85fbe
Bartek Maciag <bartekmaciag@icloud.com>
2020-12-20 19:31:38 +0100

    add basic requests

--- src/Planner/requests/task-service.js
+++ src/Planner/requests/task-service.js
@@ -8,6 +8,25 @@ class TaskService {
     hello(){
         return axios.get(API_URL+"home", {headers: authHeader()});
     }
+    getAllTasks() {
+        return axios.get(
+          API_URL + "tasks/getAllTasks",
+          {headers:  authHeader()
+          }
+        )
+      }
+    
+      addTask(taskName, taskCategory, taskDuration, taskDate) {
+        return axios.post(API_URL + "tasks/addTask",
+        { headers: authHeader(),
+          done: false,
+          name: taskName,
+          category: taskCategory,
+          duration: taskDuration,
+          date: taskDate
+        })
+      }
+  
 
 }
 export default new TaskService();
